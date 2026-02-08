662e35606b8b838d51908cfdcb9d184021c5ffd1
catskateboard <catskateboard@gmail.com>
2026-02-07 23:42:03 -0500

    Added a conclusion to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -1,10 +1,10 @@
 """
 lab_1a.py
 
-This is to simulate a change made on a robot: robot_speed = 8 # m/s
-
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+
+This is to simulate a change made on a robot: robot_speed = 8 # m/s
 """
 
 def main():
@@ -14,6 +14,8 @@ def main():
 
     print(f"{name}, Welcome to the CSS course!")
     print(f"Hello, I'm {name}, and I am a sophomore from eastern Massachusetts. I enjoy 3d printing parts of and assembling toys for my cat, and I hope this course will give me ideas on how to better my inventions and integrate computation into them.")
+    print("That was my introduction.")
+
 
 if __name__ == "__main__":
     main()
